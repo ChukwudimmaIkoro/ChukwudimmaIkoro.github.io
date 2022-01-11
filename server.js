f0b56b8c1388ab95f5ba429956f3a798ecb54260
Chukwudimma Ikoro <chuchuikoro@gmail.com>
2022-01-11 14:18:29 -0500

    Slight fixes

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require("path");
 const app = express();
 
 //When path has /static, serve static directory as per usual
-app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+app.use("/static", express.static(path.resolve(__dirname, "docs", "static")));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname, "docs", "index.html"));
 });
 
 app.listen(process.env.PORT || 8080, () => console.log("Server running..."));
