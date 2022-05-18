39a0dfed56fc382c8e8ddb5aea2e7fc021bc7ba7
Chukwudimma Ikoro <chuchuikoro@gmail.com>
2022-05-18 16:19:25 -0400

    Added tabs, fixed bugs

--- docs/static/js/views/Animation.js
+++ docs/static/js/views/Animation.js
@@ -20,7 +20,7 @@ export default class extends AbstractView {
 
 
            <iframe width="1024" height="576" 
-           src="https://www.youtube.com/embed/1sfYXsmzv-s" 
+           src="https://www.youtube.com/embed/TD9cUuaGlI0" 
            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
            clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  
 
