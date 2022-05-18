30a6ddb4477ccf4764cb911c3e1ff5c620124eb7
Chukwudimma Ikoro <chuchuikoro@gmail.com>
2022-05-18 13:10:47 -0400

    Update Dashboard.js

--- docs/static/js/views/Dashboard.js
+++ docs/static/js/views/Dashboard.js
@@ -21,7 +21,7 @@ export default class extends AbstractView {
 
                 <div class="fade-in-image">               
                     <div class="img-with-text">
-                        <a href="/portfolio/animation" class="nav__link" data-link>
+                        <base href="/portfolio/animation">
                         <img src="https://i.imgur.com/oIxz8AN.gif" title="source: imgur.com" width:800 height=600> 
                         <h2>Animation</h2>
                     </div>
