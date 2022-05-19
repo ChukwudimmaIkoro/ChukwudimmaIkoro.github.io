4c12a266a02df2fe75954c6e8d2e64d596515779
Chukwudimma Ikoro <chuchuikoro@gmail.com>
2022-05-18 21:02:58 -0400

    Commit 5/18/22

--- docs/static/js/views/Animation.js
+++ docs/static/js/views/Animation.js
@@ -23,28 +23,28 @@ export default class extends AbstractView {
            clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  
 
             <img src="docs/static/css/images/SonicRunGif.gif" 
-            style="width=1024; height="576">
+            style="width=100%; height="576">
 
             <img src="docs/static/css/images/Flour_Fight.gif" 
-            style="width=1024; height=576">
+            style="width=100%; height=576">
 
             <img src="docs/static/css/images/Art120_Gif.gif" 
-            style="width=960; height=540">
+            style="width=80%; height=540">
 
             <img src="docs/static/css/images/BallTail.gif" 
-            style="width=1024; height=576"</a>
+            style="width=100%; height=576"</a>
 
             <img src="docs/static/css/images/Sonic_Final.gif" 
-            style="width=1024; height=576">
+            style="width=100%; height=576">
    
             <img src="docs/static/css/images/flow.gif"  
-            style="width=1024; height=576">
+            style="width=100%; height=576">
 
             <img src="docs/static/css/images/owlphibia.gif" 
-            style="width=1024; height=576">
+            style="width=100%; height=576">
 
             <img src="docs/static/css/images/SnomSpin.gif" 
-            style="width=1024; height=576">
+            style="width=100%; height=576">
 
         </div>
         `;
