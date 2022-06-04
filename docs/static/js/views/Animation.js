df48f59e4a0d097ad9515052e2c01ea81ee857a9
Chukwudimma Ikoro <chuchuikoro@gmail.com>
2022-06-04 16:08:59 -0400

    Update Animation.js

--- docs/static/js/views/Animation.js
+++ docs/static/js/views/Animation.js
@@ -26,7 +26,7 @@ export default class extends AbstractView {
           
 
             <img src="docs/static/css/images/SonicRunGif.gif" 
-            style="width=100%; height=576">
+            style="width=80%; height=auto">
 
             <img src="docs/static/css/images/Flour_Fight.gif" 
             style="width=100%; height=576">
