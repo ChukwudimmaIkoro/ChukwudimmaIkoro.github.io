e9fdf2b1512f9fa72b0cc787448d3c070066d4ac
Chukwudimma Ikoro <chuchuikoro@gmail.com>
2022-06-04 17:29:08 -0400

    Update Resume.js

--- docs/static/js/views/Resume.js
+++ docs/static/js/views/Resume.js
@@ -15,13 +15,13 @@ export default class extends AbstractView {
         <h1>CS Resume</h1>
         
            <div class="container">
-           <iframe class="responsive-iframe" src="https://drive.google.com/file/d/1AIZ6sBtnlxDOZ6CRbKJdp2RhLX1Vab-q/preview" height="100%" allow="autoplay"></iframe>
+           <iframe class="responsive-iframe" src="https://drive.google.com/file/d/1AIZ6sBtnlxDOZ6CRbKJdp2RhLX1Vab-q/preview" width="100%" height="100%" allow="autoplay"></iframe>
            </div>
            
            <h1>Art Resume</h1>
            
            <div class="container">
-           <iframe class="responsive-iframe" src="https://drive.google.com/file/d/1NGbNtH4NDODOeDoOGtybTfT5xALsxdyn/preview" height="100%" allow="autoplay"></iframe>
+           <iframe class="responsive-iframe" src="https://drive.google.com/file/d/1NGbNtH4NDODOeDoOGtybTfT5xALsxdyn/preview" width="100%" height="100%" allow="autoplay"></iframe>
            </div>
            
         </div>
