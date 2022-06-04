879f9da0af5c692b7a5d36a22386e4a3af8c3fc2
Chukwudimma Ikoro <chuchuikoro@gmail.com>
2022-06-04 15:19:37 -0400

    Update Animation.js

--- docs/static/js/views/Animation.js
+++ docs/static/js/views/Animation.js
@@ -10,6 +10,7 @@ export default class extends AbstractView {
    
     async getHtml() {
         return `
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <div class="fade-in-text">
         <h2>Animation</h2>
 
